7b4186d867a485389a50365517262f0872c4373a
jose.john@yahoo.com <jose.john@yahoo.com>
2016-11-15 15:32:23 +0100

    String reverse

--- factory/src/main/java/fact/string/SpringReverse.java
+++ factory/src/main/java/fact/string/SpringReverse.java
@@ -5,7 +5,7 @@ public class SpringReverse {
 	public static void main(String[] args) {
 		System.out.println(reverseRecursively("jose"));
 	}
-	
+
 	public static String reverseRecursively(String str) {
 
         //base case to handle one char string and empty string
@@ -16,6 +16,17 @@ public class SpringReverse {
         return reverseRecursively(str.substring(1)) + str.charAt(0);
 
     }
+
+	 public static String reverse ( String s ) {
+     int length = s.length(), last = length - 1;
+     char[] chars = s.toCharArray();
+     for ( int i = 0; i < length/2; i++ ) {
+         char c = chars[i];
+         chars[i] = chars[last - i];
+         chars[last - i] = c;
+     }
+     return new String(chars);
+ }
 
 
 
